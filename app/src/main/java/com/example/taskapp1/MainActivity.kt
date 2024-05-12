e9b4074e474adc3f9441d7a152d85fce87c49fa5
Hima Alahakoon <it22181688@my.sliit.lk>
2024-05-12 16:49:51 +0530

    create database

--- app/src/main/java/com/example/taskapp1/MainActivity.kt
+++ app/src/main/java/com/example/taskapp1/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.taskapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView:RecyclerView = findViewById(R.id.rvTask)
+        val todoAdapter=TodoAdapter()
+        recyclerView.adapter= todoAdapter
+        recyclerView.layoutManager=LinearLayoutManager(this)
+
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/taskapp1/TodoAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.taskapp1
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView.Adapter
+
+
+class TodoAdapter:Adapter<TodoViewHolder>() {
+
+    lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.view_item,parent,false)
+        context=parent.context
+
+        return TodoViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+
+        return 100
+
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+
+        holder.cbTodo.text="Sample Text"
+        holder.ivDelete.setOnClickListener{
+            Toast.makeText(context,"Hello",Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
